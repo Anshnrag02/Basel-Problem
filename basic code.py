5709ebe155b03b6c329acf852ef63d57e33c137c
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2021-04-05 16:08:34 +0530

    Update basic code.py

--- basic code.py
+++ basic code.py
@@ -1,7 +1,6 @@
-import math 
-
-N=input(int()) 
-total = 0 
-for j in range(1,N): 
-  total += (1/j**2) 
-print(".\t", total) 
+import math
+N=int(input())
+total = 0
+for j in range(1,N):
+  total+= (1/j**2)
+print("\t",total)
